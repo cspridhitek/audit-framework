b9ef9f7c4f6a3cc3ae8d099988d1e91032ef9faf
csp <cspridhitek@gmail.com>
2025-03-15 21:07:18 +0530

    added validation for the parameter in the api for avoid the random value

--- src/main/java/com/ridhitek/audit/controller/AuditController.java
+++ src/main/java/com/ridhitek/audit/controller/AuditController.java
@@ -3,17 +3,19 @@ package com.ridhitek.audit.controller;
 import com.ridhitek.audit.dto.AuditLogDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.ridhitek.audit.entity.AuditLog;
 import com.ridhitek.audit.service.AuditService;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/audit")
+@Validated
 public class AuditController {
 
     private final AuditService auditService;
@@ -24,10 +26,10 @@ public class AuditController {
 
     @GetMapping
     public ResponseEntity<Page<AuditLogDTO>> getAllAuditLogs(
-            @RequestParam(name = "page", defaultValue = "0") int page,
-            @RequestParam(name = "size", defaultValue = "10") int size,
-            @RequestParam(name = "sortBy", defaultValue = "timestamp") String sortBy,
-            @RequestParam(name = "sortDirection", defaultValue = "desc") String sortDirection) {
+            @RequestParam(name = "page", defaultValue = "0") @Min(0) int page,
+            @RequestParam(name = "size", defaultValue = "10") @Min(1) int size,
+            @RequestParam(name = "sortBy", defaultValue = "timestamp") @Pattern(regexp = "^[a-zA-Z0-9_]+$") String sortBy,
+            @RequestParam(name = "sortDirection", defaultValue = "desc") @Pattern(regexp = "^(asc|desc)$") String sortDirection) {
 
         Page<AuditLog> auditLogs = auditService.getAllAuditLogs(page, size, sortBy, sortDirection);
 
@@ -42,6 +44,4 @@ public class AuditController {
                 ));
         return ResponseEntity.ok(auditLogDTOPage);
     }
-
-
 }
